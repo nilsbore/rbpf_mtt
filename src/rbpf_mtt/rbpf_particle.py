f4607400797a0aa641c79e84a5db02dfdffc0df4
Nils Bore <nilsbore@gmail.com>
2017-03-24 16:55:06 +0100

    Got something working

--- src/rbpf_mtt/rbpf_particle.py
+++ src/rbpf_mtt/rbpf_particle.py
@@ -15,6 +15,14 @@ def gauss_pdf(y, m, P):
         return 0.
     return 1./denom*math.exp(-0.5*np.dot(d, np.linalg.solve(P, d)))
 
+def gauss_expected_likelihood(m, P):
+    dim = len(m)
+    det = np.linalg.det(P)
+    if det < 0.0000001:
+        return 0.
+    denom = 2.**dim*math.sqrt((math.pi)**dim*np.linalg.det(P))
+    return 1./denom
+
 # this is in principle just a Kalman filter over all the states
 class RBPFMParticle(object):
 
@@ -251,13 +259,27 @@ class RBPFMParticle(object):
         # compute the likelihoods for all the observations and targets
         for k in range(0, nbr_targets):
 
-            target_pjump = pjump
-            target_pnone = pnone
+            # This is an absolute probability
+            target_pnone = 0.01 # This is absolute!
+            # These probabilites are proportional to each other, but also absolute
+            target_pprop = 0.95 # probability of local movement to this measurement
+            target_pjump = 0.05 # probability of jumping to this measurement
+            pdensity = 1.0
 
             if np.sum(location_ids == self.location_ids[k]) == 0: # no observations from target room
-                target_pnone = 0.1 - pnone
                 if self.might_have_jumped[k]:
-                    target_pjump = pjump / 0.1
+                    target_pnone = 1. - target_pnone - target_pprop / (1. - target_pnone) # computed so that the jump probabilities stay the same!
+                else:
+                    target_pnone = 1. - target_pnone - target_pprop / (1. - target_pnone) # computed so that the jump probabilities stay the same!
+                target_pprop = 0. # can't have any local movement here!
+
+            prior = np.zeros((2*nbr_observations+1))
+            prior_norm = nbr_observations * (target_pprop + target_pjump) / (1. - target_pnone)
+            prior[:nbr_observations] = target_pprop / prior_norm
+            prior[nbr_observations:2*nbr_observations] = target_pjump / prior_norm
+            prior[2*nbr_observations] = target_pnone
+
+            likelihood = np.zeros((2*nbr_observations+1))
 
             for j in range(0, nbr_observations):
 
@@ -280,11 +302,22 @@ class RBPFMParticle(object):
                 spatial_likelihoods[k, j] = gauss_pdf(spatial_measurements[j], self.sm[k], sS)
                 feature_likelihoods[k, j] = gauss_pdf(feature_measurements[j], self.fm[k], fS)
 
-            likelihoods[k, :nbr_observations] = spatial_likelihoods[k, :]*feature_likelihoods[k, :]
-            likelihoods[k, nbr_observations:2*nbr_observations] = pjump*feature_likelihoods[k, :]
-            likelihoods[k, 2*nbr_observations] = target_pnone
-            weights[k] = np.sum(likelihoods[k])
-            likelihoods[k] = 1./weights[k]*likelihoods[k]
+            #likelihoods[k, :nbr_observations] = spatial_likelihoods[k, :]*feature_likelihoods[k, :]
+            #likelihoods[k, nbr_observations:2*nbr_observations] = pjump*feature_likelihoods[k, :]
+            #likelihoods[k, 2*nbr_observations] = target_pnone
+            #weights[k] = np.sum(likelihoods[k])
+            #likelihoods[k] = 1./weights[k]*likelihoods[k]
+
+            spatial_expected_likelihood = gauss_expected_likelihood(self.sm[k], sS)
+            feature_expected_likelihood = gauss_expected_likelihood(self.fm[k], fS)
+
+            likelihood[:nbr_observations] = spatial_likelihoods[k, :]*feature_likelihoods[k, :]
+            likelihood[nbr_observations:2*nbr_observations] = 0.001*spatial_expected_likelihood*feature_likelihoods[k, :]
+            likelihood[2*nbr_observations] = spatial_expected_likelihood*feature_expected_likelihood*pdensity
+            proposal = prior*likelihood
+            weights[k] = np.sum(proposal)
+            likelihoods[k] = 1./weights[k]*proposal
+
 
         return likelihoods, weights, pot_sm, pot_fm, pot_sP, pot_fP
 
@@ -353,8 +386,8 @@ class RBPFMParticle(object):
         #pdclutter = 0.1 # probability density of clutter measurement
         #pjump = 0.04
         #pnone = 0.04
-        pjump = 0.001
-        pnone = 0.001
+        pjump = 0.001 #0.001
+        pnone = 0.0001 #0.001
         spatial_var = self.spatial_std*self.spatial_std
         feature_var = self.feature_std*self.feature_std
 
@@ -397,169 +430,3 @@ class RBPFMParticle(object):
                 #weights_update *= 1.0#likelihoods[k, i]/pc[k, i]
 
         return np.prod(weights)
-
-
-    def meas_compute_update(self, spatial_measurements, feature_measurements,
-                            pclutter, pdclutter, pjump, sR, fR):
-
-        nbr_targets = self.sm.shape[0]
-        spatial_dim = self.sm.shape[1]
-        feature_dim = self.fm.shape[1]
-        nbr_observations = spatial_measurements.shape[0]
-
-        # Can we have 4-dimensional matrix in numpy, YES WE CAN!
-        pot_sm = np.zeros((nbr_observations, nbr_targets, spatial_dim)) # the Kalman filter means
-        pot_fm = np.zeros((nbr_observations, nbr_targets, feature_dim))
-        pot_sP = np.zeros((nbr_observations, nbr_targets, spatial_dim, spatial_dim)) # the Kalman filter covariances
-        pot_fP = np.zeros((nbr_observations, nbr_targets, feature_dim, feature_dim))
-        likelihoods = np.zeros((nbr_observations, nbr_targets+1))
-        pc = np.zeros((nbr_observations, nbr_targets+2))
-
-        spatial_likelihoods = np.zeros((nbr_observations, nbr_targets))
-        feature_likelihoods = np.zeros((nbr_observations, nbr_targets))
-
-        # compute the likelihoods for all the observations and targets
-        for k in range(0, nbr_observations):
-            for j in range(0, nbr_targets):
-
-                sy = spatial_measurements[k] - self.sm[j]
-                fy = feature_measurements[k] - self.fm[j]
-
-                sS = self.sP[j] + sR # IS = (R + H*P*H');
-                fS = self.fP[j] + fR
-
-                # sP^T = sP, sR^t = sR -> sS^-1*sP = sP*sS^-1
-                sK = np.linalg.solve(sS, self.sP[j]) # K = P*H'/IS;
-                fK = np.linalg.solve(fS, self.fP[j])
-
-                pot_sm[k, j] = self.sm[j] + np.dot(sK, sy) # X = X + K * (y-IM);
-                pot_fm[k, j] = self.fm[j] + np.dot(fK, fy)
-                pot_sP[k, j] = np.dot((np.identity(spatial_dim) - sK), self.sP[j])
-                pot_fP[k, j] = np.dot((np.identity(feature_dim) - fK), self.fP[j])
-
-                # TODO: wait 1s, didn't they write that this should be pot_sm[j]?
-                spatial_likelihoods[k, j] = gauss_pdf(spatial_measurements[k], self.sm[j], sS)
-                feature_likelihoods[k, j] = gauss_pdf(feature_measurements[k], self.fm[j], fS)
-
-            likelihoods[k, :nbr_targets] = spatial_likelihoods[k, :]*feature_likelihoods[k, :]
-            likelihoods[k, nbr_targets] = pdclutter
-            #likelihoods = 1./np.sum(likelihoods)*likelihoods
-
-            # probability of measurement given association, may arise from clutter anyways?
-
-            # let's
-
-            pc[k, :nbr_targets] = (1.-pclutter)*likelihoods[k, :nbr_targets] # + pclutter?
-            pc[k, nbr_targets] = pclutter*likelihoods[k, nbr_targets]
-            pc[k, nbr_targets+1] = pjump
-            pc[k] = 1./np.sum(pc[k])*pc[k]
-
-            #for picked in self.c:
-            #    pc[k, picked] = 0.
-            #pc = 1./np.sum(pc)*pc
-
-        return likelihoods, feature_likelihoods, pc, pot_sm, pot_fm, pot_sP, pot_fP
-
-
-    def meas_sample_update(self, nbr_observations, pc):
-
-        nbr_targets = self.sm.shape[0]
-        spatial_dim = self.sm.shape[1]
-        feature_dim = self.fm.shape[1]
-
-        # sample measurement -> target mapping
-        states = np.zeros((nbr_observations,), dtype=int)
-        while True:
-            for k in range(0, nbr_observations):
-                # the problem here is that it does not take the other probabilites into account
-                states[k] = np.random.choice(nbr_targets+2, p=pc[k])
-            unique, counts = np.unique(states, return_counts=True)
-            unique_list = unique.tolist()
-            nbr_sampled_noise = 0
-            if nbr_targets in unique_list:
-                nbr_sampled_noise = counts[unique_list.index(nbr_targets)] - 1
-            nbr_sampled_jumps = 0
-            if nbr_targets+1 in unique_list:
-                nbr_sampled_jumps = counts[unique_list.index(nbr_targets+1)] - 1
-            print "Nbr sampled noise, jumps, targets: ", nbr_sampled_noise, nbr_sampled_jumps, nbr_targets
-            if len(unique) + nbr_sampled_jumps > nbr_targets:
-                print "Sampled more jumps and associations than possible..."
-            elif len(unique) + nbr_sampled_noise + nbr_sampled_jumps == nbr_observations:
-                for j in unique_list:
-                    if j != nbr_targets and j != nbr_targets+1:
-                        self.c.append(j) # we need to this this here to be sure to not sample jumps later
-                break
-            else:
-                print "Found several similar states: ", states
-
-        return states
-
-    # this functions takes in several measurements at the same timestep
-    # and does association jointly, leading to fewer particles with low weights
-    def meas_joint_update(self, spatial_measurements, feature_measurements, time, observation_id):
-
-        self.c = []
-        self.did_jump = False
-
-        if time != self.last_time:
-            self.c = []
-            self.last_time = time
-
-        print "Got measurement with observations: ", spatial_measurements.shape[0]
-
-        # we somehow need to integrate the feature density into these clutter things
-        pclutter = 0.1#0.3 # probability of measurement originating from noise
-        pdclutter = 0.001#0.1 # probability density of clutter measurement
-        pjump = 0.001#0.1
-        spatial_var = self.spatial_std*self.spatial_std
-        feature_var = self.feature_std*self.feature_std
-
-        nbr_targets = self.sm.shape[0]
-        spatial_dim = self.sm.shape[1]
-        feature_dim = self.fm.shape[1]
-        nbr_observations = spatial_measurements.shape[0]
-
-        sR = spatial_var*np.identity(spatial_dim) # measurement noise
-        fR = feature_var*np.identity(feature_dim) # measurement noise
-
-        likelihoods, feature_likelihoods, pc, pot_sm, pot_fm, pot_sP, pot_fP = \
-            self.meas_compute_update(spatial_measurements, feature_measurements,
-                                     pclutter, pdclutter, pjump, sR, fR)
-
-        states = self.meas_sample_update(nbr_observations, pc)
-
-        weights_update = 1.
-
-        for k, i in enumerate(states):
-
-            if i == nbr_targets:
-                self.nbr_noise += 1
-                self.c.append(nbr_targets) # to know that we observed noise!
-                weights_update *= likelihoods[k, i]/pc[k, i]
-            elif i == nbr_targets+1:
-                feature_likelihoods_k = feature_likelihoods[k]
-                for j in filter(lambda x: x != nbr_targets, self.c): # filter noise
-                    if j < nbr_targets: # normal association
-                        feature_likelihoods_k[j] = 0.
-                    else: # jump association
-                        feature_likelihoods_k[j-nbr_targets-1] = 0.
-                feature_likelihoods_k = feature_likelihoods_k/np.sum(feature_likelihoods[k])
-                i = np.random.choice(nbr_targets, p=feature_likelihoods_k)
-                self.c.append(nbr_targets + 1 + i) # offset for jump objects
-                self.sm[i] = spatial_measurements[k]
-                self.sP[i] = spatial_var*np.eye(spatial_dim)
-                self.did_jump = True
-                self.nbr_jumps += 1
-                self.target_jumps[i] += 1
-                weights_update *= 1*pjump # 0.1*pjump
-            else:
-                self.sm[i] = pot_sm[k, i]
-                self.fm[i] = pot_fm[k, i]
-                self.sP[i] = pot_sP[k, i]
-                self.fP[i] = pot_fP[k, i]
-                #self.c.append(i) # this can't be here since we might sample a jump to this before then
-                self.associations[observation_id] = i
-                self.nbr_assoc += 1
-                weights_update *= likelihoods[k, i]/pc[k, i]
-
-        return weights_update
